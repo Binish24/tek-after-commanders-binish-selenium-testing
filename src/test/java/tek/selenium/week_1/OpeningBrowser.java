cc53c6a1f1073785c83f21d88f357e68c2418cdd
nabiullahbenish <nabiullah_benash@yahoo.com>
2025-04-14 10:14:28 -0700

    Opening Browser

---
+++ src/test/java/tek/selenium/week_1/OpeningBrowser.java
@@ -0,0 +1,25 @@
+package tek.selenium.week_1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+
+public class OpeningBrowser {
+
+    public static void main(String[] args) {
+
+   // WebDriver driver = new ChromeDriver();
+    ChromeDriver chromeDriver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
+
+//    chromeDriver.get("https://www.facebook.com/");
+//    chromeDriver.get("https://naraiman.com/");
+
+//    driver.get("https://www.bbc.com/persian");
+        driver.get("https://da.azadiradio.com/");
+
+
+}
+
+
+}
